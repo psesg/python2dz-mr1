feddc324bca9c24389ee31cb2f45ff79b02b7ff6
PanarinSE <pseesp@mail.ru>
2022-08-21 17:10:09 +0300

    mr1 -ok (python2) https://docs.anaconda.com/anaconda/user-guide/tasks/switch-environment/

--- reducer_py2.py
+++ reducer_py2.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
 import sys
-import string
 import random
 
 # python mapper_py2.py < part1.txt | sort | python reducer_py2.py | head -10000
 # if head < 10000 will be "BrokenPipeError: [Errno 32] Broken pipe" generated
 
-#reload(sys)
-#sys.setdefaultencoding('utf-8')  # required to convert to unicode
+reload(sys)
+sys.setdefaultencoding('utf-8')  # required to convert to unicode
 
 i = 1
 rnd_num = random.randint(1, 5)
 out_str = ""
-print "first rand = %d" % rnd_num
+
 for line in sys.stdin:
     try:
-        line = sys.stdin.readline()
+        line = unicode(line.strip("\n"))
+        line = unicode(line.strip("\t"))
     except ValueError as e:
         continue
     else:
-        line = line.strip("\n")[5:]
+        line = line[5:]
+
         if i == rnd_num:
             out_str = out_str + line
             print "%s" % out_str
             i = 1
             rnd_num = random.randint(1, 5)
             out_str = ""
-            print "rand = %d" % rnd_num
         else:
             out_str = out_str + line + ","
             i += 1
